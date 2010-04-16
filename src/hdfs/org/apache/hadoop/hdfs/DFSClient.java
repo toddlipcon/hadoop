e83301f8cb7d24c4e1c546fbd521b185a585d6d4
Todd Lipcon <todd@cloudera.com>
2010-04-16 15:11:39 -0700

    HDFS-895. Slightly improved test case, restructuring a little closer to trunk

--- src/hdfs/org/apache/hadoop/hdfs/DFSClient.java
+++ src/hdfs/org/apache/hadoop/hdfs/DFSClient.java
@@ -3240,9 +3240,9 @@ public class DFSClient implements FSConstants, java.io.Closeable {
     }
 
     private void waitForAckedSeqno(long seqnumToWaitFor) throws IOException {
-      while (!closed) {
-        isClosed();
-        synchronized (ackQueue) {
+      synchronized (ackQueue) {
+        while (!closed) {
+          isClosed();
           if (lastAckedSeqno >= seqnumToWaitFor) {
             break;
           }
@@ -3251,6 +3251,7 @@ public class DFSClient implements FSConstants, java.io.Closeable {
           } catch (InterruptedException ie) {}
         }
       }
+      isClosed();
     }
  
     /**
--- src/test/org/apache/hadoop/hdfs/TestMultiThreadedSync.java
+++ src/test/org/apache/hadoop/hdfs/TestMultiThreadedSync.java
@@ -17,41 +17,48 @@
  */
 package org.apache.hadoop.hdfs;
 
-import junit.framework.TestCase;
+import org.junit.Test;
+
 import java.io.*;
-import java.net.*;
 import java.util.ArrayList;
-import java.util.List;
+import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.atomic.AtomicReference;
 
+import org.apache.commons.logging.impl.Log4JLogger;
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.FileSystem;
 import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.fs.FSDataOutputStream;
-import org.apache.hadoop.fs.FSDataInputStream;
-import org.apache.hadoop.fs.FileUtil.HardLink;
-import org.apache.hadoop.fs.BlockLocation;
-import org.apache.hadoop.hdfs.protocol.Block;
-import org.apache.hadoop.hdfs.protocol.LocatedBlock;
-import org.apache.hadoop.hdfs.protocol.LocatedBlocks;
 import org.apache.hadoop.hdfs.server.datanode.DataNode;
-import org.apache.hadoop.hdfs.server.datanode.FSDataset;
-import org.apache.hadoop.hdfs.server.datanode.SimulatedFSDataset;
-
-import org.apache.commons.logging.impl.Log4JLogger;
+import org.apache.hadoop.hdfs.server.namenode.FSNamesystem;
+import org.apache.hadoop.hdfs.server.namenode.LeaseManager;
+import org.apache.hadoop.hdfs.server.namenode.NameNode;
+import org.apache.hadoop.hdfs.server.protocol.InterDatanodeProtocol;
 import org.apache.log4j.Level;
+
 /**
  * This class tests the building blocks that are needed to
  * support HDFS appends.
  */
-public class TestMultiThreadedSync extends TestCase {
+public class TestMultiThreadedSync {
   static final int blockSize = 1024*1024;
   static final int numBlocks = 10;
   static final int fileSize = numBlocks * blockSize + 1;
 
-  private long seed;
+  private static final int NUM_THREADS = 10;
+  private static final int WRITE_SIZE = 517;
+  private static final int NUM_WRITES_PER_THREAD = 1000;
+  
   private byte[] toWrite = null;
 
+  {
+    ((Log4JLogger)NameNode.stateChangeLog).getLogger().setLevel(Level.ALL);
+    ((Log4JLogger)LeaseManager.LOG).getLogger().setLevel(Level.ALL);
+    ((Log4JLogger)FSNamesystem.LOG).getLogger().setLevel(Level.ALL);
+    ((Log4JLogger)DataNode.LOG).getLogger().setLevel(Level.ALL);
+    ((Log4JLogger)DFSClient.LOG).getLogger().setLevel(Level.ALL);
+    ((Log4JLogger)InterDatanodeProtocol.LOG).getLogger().setLevel(Level.ALL);
+  }
 
   /*
    * creates a file but does not close it
@@ -73,16 +80,20 @@ public class TestMultiThreadedSync extends TestCase {
     private final FSDataOutputStream stm;
     private final AtomicReference<Throwable> thrown;
     private final int numWrites;
+    private final CountDownLatch countdown;
 
     public WriterThread(FSDataOutputStream stm,
-      AtomicReference<Throwable> thrown, int numWrites) {
+      AtomicReference<Throwable> thrown,
+      CountDownLatch countdown, int numWrites) {
       this.stm = stm;
       this.thrown = thrown;
       this.numWrites = numWrites;
+      this.countdown = countdown;
     }
 
     public void run() {
       try {
+        countdown.await();
         for (int i = 0; i < numWrites && thrown.get() == null; i++) {
           doAWrite();
         }
@@ -97,13 +108,15 @@ public class TestMultiThreadedSync extends TestCase {
     }
   }
 
-  public void testMutipleSyncers() throws Exception {
+
+  @Test
+  public void testMultipleSyncers() throws Exception {
     Configuration conf = new Configuration();
     MiniDFSCluster cluster = new MiniDFSCluster(conf, 1, true, null);
     FileSystem fs = cluster.getFileSystem();
     Path p = new Path("/multiple-syncers.dat");
     try {
-      doMultithreadedWrites(conf, p, 10, 7, 10000);
+      doMultithreadedWrites(conf, p, NUM_THREADS, WRITE_SIZE, NUM_WRITES_PER_THREAD);
     } finally {
       fs.close();
       cluster.shutdown();
@@ -120,25 +133,32 @@ public class TestMultiThreadedSync extends TestCase {
     FSDataOutputStream stm = createFile(fs, p, 1);
     System.out.println("Created file simpleFlush.dat");
 
-    stm.sync();
+    // TODO move this bit to another test case
+    // There have been a couple issues with flushing empty buffers, so do
+    // some empty flushes first.
     stm.sync();
     stm.sync();
     stm.write(1);
     stm.sync();
     stm.sync();
 
+    CountDownLatch countdown = new CountDownLatch(1);
     ArrayList<Thread> threads = new ArrayList<Thread>();
     AtomicReference<Throwable> thrown = new AtomicReference<Throwable>();
     for (int i = 0; i < numThreads; i++) {
-      Thread t = new WriterThread(stm, thrown, numWrites);
+      Thread t = new WriterThread(stm, thrown, countdown, numWrites);
       threads.add(t);
       t.start();
     }
 
+    // Start all the threads at the same time for maximum raciness!
+    countdown.countDown();
+    
     for (Thread t : threads) {
       t.join();
     }
     if (thrown.get() != null) {
+      
       throw new RuntimeException("Deferred", thrown.get());
     }
     stm.close();
