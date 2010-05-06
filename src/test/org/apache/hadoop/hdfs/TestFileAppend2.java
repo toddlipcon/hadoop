90d5408e1c6ed06e76bcac730736f74453d5be1b
Todd Lipcon <todd@cloudera.com>
2010-05-06 08:52:18 -0700

    Add some timeouts to TestFileAppend2 to catch timeout happening on build machines

--- src/test/org/apache/hadoop/hdfs/TestFileAppend2.java
+++ src/test/org/apache/hadoop/hdfs/TestFileAppend2.java
@@ -21,7 +21,8 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Arrays;
 
-import junit.framework.TestCase;
+import org.junit.Test;
+import static org.junit.Assert.*;
 
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.FSDataInputStream;
@@ -46,7 +47,7 @@ import org.apache.log4j.Level;
  * This class tests the building blocks that are needed to
  * support HDFS appends.
  */
-public class TestFileAppend2 extends TestCase {
+public class TestFileAppend2 {
 
   {
     ((Log4JLogger)NameNode.stateChangeLog).getLogger().setLevel(Level.ALL);
@@ -122,6 +123,7 @@ public class TestFileAppend2 extends TestCase {
    * Reopens the same file for appending, write all blocks and then close.
    * Verify that all data exists in file.
    */ 
+  @Test(timeout=200000)
   public void testSimpleAppend() throws IOException {
     Configuration conf = new Configuration();
     if (simulatedStorage) {
@@ -327,6 +329,7 @@ public class TestFileAppend2 extends TestCase {
           stm.close();
 
           // wait for the file size to be reflected in the namenode metadata
+          long startWaitTime = System.currentTimeMillis();
           while (fs.getFileStatus(testfile).getLen() != (len + sizeToAppend)) {
             try {
               System.out.println("Workload thread " + id +
@@ -335,6 +338,10 @@ public class TestFileAppend2 extends TestCase {
                                  " expected size " + (len + sizeToAppend) +
                                  " waiting for namenode metadata update.");
               Thread.sleep(5000);
+              assertTrue("Timed out waiting for len " + (len + sizeToAppend) +
+                " in file " + testfile + " (cur len is " +
+                fs.getFileStatus(testfile).getLen() + ")",
+                System.currentTimeMillis() - startWaitTime < 60000);
             } catch (InterruptedException e) { 
             }
           }
@@ -369,6 +376,7 @@ public class TestFileAppend2 extends TestCase {
   /**
    * Test that appends to files at random offsets.
    */
+  @Test
   public void testComplexAppend() throws IOException {
     initBuffer(fileSize);
     Configuration conf = new Configuration();
